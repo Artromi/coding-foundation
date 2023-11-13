c77ac8a14ded8beea5ec942b82617dfe47d03e1f
Ari <arisenger0@gmail.com>
2023-11-13 16:48:21 +0100

    coding task 02

---
+++ script.js
@@ -0,0 +1,20 @@
+// single line comment
+
+/* 
+   multi 
+   line 
+   comment
+*/
+
+console.log("Hello, World!");
+console.log(1 + 1);
+console.log(3 * 4);
+console.log(12 / 3);
+console.log(5 - 4);
+
+console.log(1 == 1); //true
+console.log(1 != 1); //false
+console.log(1 > 1); //false
+console.log(1 >= 1); //true
+console.log(1 < 1); //false
+console.log(1 <= 1); //true
