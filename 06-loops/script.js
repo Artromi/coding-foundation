0b6138e30a8c2d7c1a92cf1cd3fabcfc69729ac0
Ari <arisenger0@gmail.com>
2023-11-20 09:55:55 +0100

    add task 02 in loops

--- 06-loops/script.js
+++ 06-loops/script.js
@@ -16,3 +16,29 @@ function oddNumbers(num1, num2) {
 console.log(oddNumbers(0, 4));
 console.log(oddNumbers(10, 33));
 console.log(oddNumbers(9, 12));
+
+// TASK 02
+function charCount(word, char) {
+  word = word.toLowerCase();
+  char = char.toLowerCase();
+  let result = "";
+  for (let index = 0; index < word.length; index++) {
+    if (char > 1) {
+      break;
+    }
+    const currentChar = word[index];
+    if (currentChar === char) {
+      result = result + char;
+    }
+  }
+  return result.length;
+}
+
+console.log(charCount("hello", "L"));
+// result should be: 2
+
+console.log(charCount("maMa", "m"));
+// result should be: 2
+
+console.log(charCount("Resümee", "e"));
+// result should be: 3
