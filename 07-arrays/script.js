5ccd6d70441322f65e0fddcf7ca35b36ed29f5f6
Ari <arisenger0@gmail.com>
2023-11-21 10:18:23 +0100

    add task 02

--- 07-arrays/script.js
+++ 07-arrays/script.js
@@ -14,3 +14,31 @@ console.log(removeItem(fruits, 3));
 console.log(fruits);
 
 // TASK 02
+//check each array item  if string
+//count characters
+//store count of characters
+//return total sum of all characters
+
+function sumOfCharacters(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      count = count + arr[i].length;
+    }
+  }
+  return count;
+}
+
+const arr1 = ["Luke", "Anakin", true, "Obi Wan", 333];
+console.log(sumOfCharacters(arr1));
+// result should be: 17
+
+const arr2 = [
+  "Code is",
+  "like humor",
+  ".",
+  "When you have",
+  "to explain it, it's bad!",
+];
+console.log(sumOfCharacters(arr2));
+// result should be: 55
